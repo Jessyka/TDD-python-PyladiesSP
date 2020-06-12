eb7cac40093cca66ca37c4ddb22b42d803783dd6
Jessyka Lage <jessykalage@gmail.com>
2020-06-12 17:34:10 -0300

    buzz function, fizzbuzz function and refactor

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,4 +1,17 @@
 class FizzBuzz:
     def converter(self, numero):
-        if numero % 3 == 0:
-            return 'fizz'
+        saida = ''
+
+        if self.eh_multiplo_de_3(numero):
+            saida += 'fizz'
+        if self.eh_multiplo_de_5(numero):
+            saida += 'buzz'
+        if saida == '':
+            saida += str(numero)
+        return saida
+
+    def eh_multiplo_de_3(self, numero):
+        return numero % 3 == 0
+
+    def eh_multiplo_de_5(self, numero):
+        return numero % 5 == 0
--- test/test_fizzbizz.py
+++ test/test_fizzbizz.py
@@ -1,6 +1,17 @@
 from src.fizzbuzz import FizzBuzz
 
-
 def test_deve_retornar_fizz_quando_multiplo_de_3():
     fizzbuzz = FizzBuzz()
     assert 'fizz' == fizzbuzz.converter(3)
+
+def test_deve_retornar_buzz_quando_multiplo_de_5():
+    fizzbuzz = FizzBuzz()
+    assert 'buzz' == fizzbuzz.converter(5)
+
+def test_deve_retornar_fizzbuzz_quando_multiplo_de_3_e_5():
+    fizzbuzz = FizzBuzz()
+    assert 'fizzbuzz' == fizzbuzz.converter(15)
+
+def test_deve_retornar_o_proprio_numero_quando_nao_multiplo_de_3_ou_5():
+    fizzbuzz = FizzBuzz()
+    assert '1' == fizzbuzz.converter(1)
